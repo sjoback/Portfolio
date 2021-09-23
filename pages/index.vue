db6f7fa2cf64e55f88f3def4bae9f616c31be8c7
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2021-09-23 16:19:35 +0200

    23/9

--- pages/index.vue
+++ pages/index.vue
@@ -38,14 +38,57 @@
 
         <section>
             <div class="section-inner">
-                <div class="container">
-                    <img src="~/assets/images/laptop.png" alt=""/>
-                </div>
-                <div class="container">
+                <div class="grid">
+                    <div
+                        v-addClass="{threshold: .5, class: 'animate'}"
+                        class="grid-inner"
+                    >
+                        <div class="container">
+                            <img src="~/assets/images/work-kreditkort.png" alt=""/>
+                        </div>
+                        <div class="container">
+                            <div>
+                                <div class="container-head">
+                                    <h2>Kreditkort.com</h2>
+                                    <span>2020</span>
+                                </div>
 
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                                <a href="https://kreditkort.com" target="_blank">Visit site</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        v-addClass="{threshold: .5, class: 'animate'}"
+                        class="grid-inner"
+                    >
+                        <div class="container">
+                            <img src="~/assets/images/work-bettingguide.png" alt=""/>
+                        </div>
+                        <div class="container">
+                            <div>
+                                <div class="container-head">
+                                    <h2>Bettingguide.com</h2>
+                                    <span>2020</span>
+                                </div>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
+
+        <footer>
+            <div class="footer-inner">
+                <h1>K.I.S.S</h1>
+                <h2>Keep it simple, stupid!</h2>
+                <span>Keeping it simple since 1990.</span>
+            </div>
+        </footer>
     </div>
 </template>
 <script>
@@ -93,7 +136,6 @@ export default {
     }
 
     section {
-
         &:nth-child(2) {
             position: relative;
 
@@ -164,14 +206,91 @@ export default {
         }
 
         &:nth-child(4) {
-            @include grid(2, 40px);
+            .grid {
+                @include grid(1, 160px);
 
-            .container {
-                img {
-                    width: 400px;
-                    height: auto;
+                &-inner.animate {
+                    opacity: 1;
+                    animation: animate .3s ease-in forwards;
+                }
+                &-inner {
+                    opacity: 0;
+                    @include grid(2, 40px);
+
+                    .container {
+                        img { @include size(500px, auto); }
+
+                        &:last-child {
+                            @include flex;
+
+                            div { @include flex(start); }
+
+                            .container-head {
+                                @include flex(start);
+
+                                h2 {
+                                    text-align: left;
+                                    width: auto;
+                                    margin: 0;
+                                }
+
+                                span {
+                                    background: $purple;
+                                    padding: 5px;
+                                    border-radius: 3px;
+                                    color: $white;
+                                    font-size: 12px;
+                                    margin-left: 20px;
+                                    font-family: $fp;
+                                    font-weight: bold;
+                                }
+                            }
+
+                            p {
+                                @include spacing(margin,6,(bottom,top));
+                            }
+
+                            a {
+                                background: $purple;
+                                padding: 15px 30px;
+                                font-family: $fp;
+                                letter-spacing: .5px;
+                                color: $white;
+                                border-radius: 50px;
+                                transition: .15s ease-in-out;
+
+                                &:hover { background: $darkpurple; }
+                            }
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    footer {
+        @include flex;
+        background: $purple;
+        padding: 80px 20px;
+        width: 100%;
+        box-sizing: border-box;
+
+        .footer-inner {
+            width: 1000px;
+            @include flex;
+            max-width: 100%;
+            color: $white;
+        }
+    }
+
+    @keyframes animate {
+        from {
+            opacity: 0;
+            transform: translateY(50px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0px);
         }
     }
 </style>
