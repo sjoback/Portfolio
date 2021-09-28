aeeb52bde8b8e1b45061211ba6e65ef64767e0f0
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2021-09-28 14:14:54 +0200

    Update TheFooter.vue

--- components/includes/TheFooter.vue
+++ components/includes/TheFooter.vue
@@ -1,8 +1,24 @@
 <template>
     <footer>
         <div class="footer-inner">
-            <h2>A Journey of a Thousand Miles Begins with a Single Step</h2>
-            <span>Or you know.. buying a plane ticket</span>
+            <div class="footer-inner__outro">
+                <h2>A Journey of a Thousand Miles Begins with a Single Step</h2>
+                <span>Or you know.. buying a plane ticket</span>
+            </div>
+
+            <ul class="footer-inner__socials">
+                <li
+                    v-for="social in socials"
+                    :key="social"
+                >
+                    <a
+                        :href="social.url"
+                        :alt="social.name"
+                    >
+                        <i :class="social.icon"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
     </footer>
 </template>
@@ -46,6 +62,33 @@ footer {
         @include flex;
         max-width: 100%;
         color: $white;
+
+        &__outro {
+            @include grid(1, 20px);
+            width: 100%;
+            text-align: center;
+        }
+
+        &__socials {
+            @include grid(3, 30px);
+            @include spacing(margin,8,top);
+
+            li {
+                @include size(60px);
+                @include border-radius(100px);
+                @include border(all, 2px, $darkpurple);
+                transition: .15s ease-in-out;
+
+                &:hover { background: $darkpurple; }
+
+                a {
+                    color: $white;
+                    @include flex;
+                    @include size(100%);
+                    font-size: 22px;
+                }
+            }
+        }
     }
 }
 </style>
